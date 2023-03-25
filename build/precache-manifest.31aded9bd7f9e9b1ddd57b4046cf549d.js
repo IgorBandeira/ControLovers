306bdbb95c3ad79a2877f42489eaecbc6cea3804
Igor Bandeira <igorbandeirasouza03@gmail.com>
2023-03-25 17:23:02 -0300

    ControLovers

---
+++ build/precache-manifest.31aded9bd7f9e9b1ddd57b4046cf549d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6bf043df48fcb3ab69c",
+    "url": "/static/css/main.6ae18da0.chunk.css"
+  },
+  {
+    "revision": "a6bf043df48fcb3ab69c",
+    "url": "/static/js/main.a6bf043d.chunk.js"
+  },
+  {
+    "revision": "6d8f048ca440f6c8cad0",
+    "url": "/static/js/1.6d8f048c.chunk.js"
+  },
+  {
+    "revision": "597b7bb5c9fc77e841a9",
+    "url": "/static/js/2.597b7bb5.chunk.js"
+  },
+  {
+    "revision": "2c25256ec6a9a00c3a1b",
+    "url": "/static/js/runtime~main.2c25256e.js"
+  },
+  {
+    "revision": "444380132bbdb3666a34780f444f8f54",
+    "url": "/index.html"
+  }
+];
